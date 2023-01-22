ecd26e6518ce7e4d06caac77f3048c0c1ea7ea08
Lecture <Lecture@LPL-DESIGN-UEFI>
2023-01-22 15:30:11 +0600

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Delegate
+{
+    //public delegate int AddDelegate(int a, int b);
+    public delegate void RectDel(double x, double y);
+
+    internal class Program
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+
+        {
+            var reult=Add(10, 20);
+            //AddDelegate newResult = new AddDelegate(Add);
+            //var result = newResult(80,20);
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+
+
+            Rectangle rect= new Rectangle();
+            RectDel rectDel = new RectDel(rect.GetArea);
+            rectDel += rect.GetPerimeter;
+            rectDel.Invoke(17.77, 40.22);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Delegate/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    internal class Rectangle
+    {
+        public void GetArea(double width,double hight)
+        {
+            Console.WriteLine("Area of Rectangle : " + width * hight);
+        }
+
+        public void GetPerimeter(double width, double hight)
+        {
+            Console.WriteLine("Perimeter of Rectangle : " + width * hight);
+        }
+
+    }
+}
